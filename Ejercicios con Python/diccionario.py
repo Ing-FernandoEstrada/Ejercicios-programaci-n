bdb55bb2b8547284d4476061fc6644c0626916f8
Fernando <edestrada@umariana.edu.co>
2023-08-14 16:17:16 -0500

    primer commit

---
+++ Ejercicios con Python/diccionario.py
@@ -0,0 +1,17 @@
+# Animales
+#pez = 'Pez espada'
+#anfibio = 'Rana'
+#reptil = 'Tortuga'
+#ave = 'Avestrus'
+#mamifero = 'Elefante'
+
+# Los diccionarios se agregan con {}
+animales = {
+    'pez': 'Pez espada',
+    'anfibio': 'Rana',
+    'reptil': 'Tortuga',
+    'ave': 'Avestrus',
+    'mamifero': 'Elefante'
+}
+
+
--- Ejercicios con Python/lista.py
+++ Ejercicios con Python/lista.py
@@ -1,5 +1,12 @@
-ciudad1 = 'Pasto'
-ciudad2 = 'Cartagena'
-ciudad3 = 'Medellin'
-ciudad4 = 'Bogota'
-ciudad5 = 'Barranquilla'
+# ciudad1 = 'Pasto'
+# ciudad2 = 'Cartagena'
+# ciudad3 = 'Medellin'
+# ciudad4 = 'Bogota'
+
+# Se ha organizadoe el codigo y los valores estan relacionados
+ciudades = [
+    'Pasto',
+    'Cartagena',
+    'Medellin',
+    'Bogota'
+]

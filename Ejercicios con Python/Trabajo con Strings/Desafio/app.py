8ff72c21df4d1d80e8350eda406e762a4e3d5eab
Fernando <edestrada@umariana.edu.co>
2023-08-18 11:42:13 -0500

    cambios

--- Ejercicios con Python/Trabajo con Strings/Desafio/app.py
+++ Ejercicios con Python/Trabajo con Strings/Desafio/app.py
@@ -17,3 +17,10 @@ area_tringulo = (valor_area*valor_altura)/2
 print('El valor del area del tringulo es: ' )
 print(area_tringulo)
 
+def calcular_area_cuadrado(lado):
+    calcular = lado**2
+    return calcular
+
+lado = float(input('Digite la longitud del cuadrado'))
+calcular_area = calcular_area_cuadrado(lado)
+print(f"El resultado del area del cuadrado es: {calcular_area}")
